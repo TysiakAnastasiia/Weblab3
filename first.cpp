d0dd2d654ff806a3971da5eff27bd5a3c847e234
Anastasiia Tysiak <tysiaknastia@gmail.com>
2023-10-09 20:22:53 +0300

    first code

---
+++ first.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+ string greeting = "Hello!This is our assortment list";
+   cout << greeting <<endl; 
+   string first = "Yogurt - ";
+   cout << first;
+   string xy ="58₴";
+   cout << xy <<endl;
+    string second = "Bread - ";
+   cout << second;
+   string xb ="18₴";
+   cout << xb <<endl;
+    string third = "Bar -  ";
+   cout << third;
+   string x ="12₴";
+   cout << x <<endl;
+    string forth = "Buckwheat - ";
+   cout << forth;
+   string xg ="40₴";
+   cout << xg <<endl;
+    string fifth = "Eggs - ";
+   cout << fifth;
+   string xe ="52₴";
+   cout << xe <<endl;
+       string sixth = "Cheese - ";
+   cout << sixth;
+   string xc ="60₴";
+   cout << xc <<endl;
+    return 0;
+}
